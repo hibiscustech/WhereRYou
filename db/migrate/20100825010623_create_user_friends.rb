737bc0c0736b097ae2b909a2c9ced2eef827462b
harryrokin13 <Harry@.(none)>
2010-08-25 13:28:17 -0700

    migrate

---
+++ db/migrate/20100825010623_create_user_friends.rb
@@ -0,0 +1,19 @@
+class CreateUserFriends < ActiveRecord::Migration
+  def self.up
+
+ execute %Q{ CREATE TABLE `user_friends` (
+             `id` int(11) unsigned NOT NULL auto_increment,
+             `user_id` int(11) unsigned NOT NULL,
+             `friend_id` int(11) unsigned default NULL,
+              PRIMARY KEY  (`id`),
+              KEY `user_id` (`user_id`),
+              KEY `friend_id` (`friend_id`),
+              CONSTRAINT `FK_user_friends_1` FOREIGN KEY (`friend_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+            ) ENGINE=InnoDB }
+    
+  end
+
+  def self.down
+    drop_table :user_friends
+  end
+end
