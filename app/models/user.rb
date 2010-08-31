3234cef445281ada0a68437b022d1bcfebcbbac2
hibiscustech <relton@hibiscustech.com>
2010-08-31 17:27:24 +0530

    api changes

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  has_many :user_friends
+  has_many :user_locations
+
+  attr_reader :password
+
+  def password=(pass)
+    @password = pass
+    salt = [Array.new(6){rand(256).chr}.join].pack("m").chomp
+    self.password_salt, self.password_hash = salt, Digest::SHA256.hexdigest(pass + salt)
+  end
+
+  def self.check_user(username,password)
+    user = User.find(:first, :conditions => { :username => username})
+
+    if user.blank? || Digest::SHA256.hexdigest(password + user.password_salt) != user.password_hash
+      return nil
+    end
+
+    return user
+  end
+
+end
