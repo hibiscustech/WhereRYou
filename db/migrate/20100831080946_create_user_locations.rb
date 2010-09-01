4dec111626ed63761c5033469c8efa1ac11b9a91
harryrokin13 <Harry@.(none)>
2010-08-31 17:40:04 -0700

    db changes

---
+++ db/migrate/20100831080946_create_user_locations.rb
@@ -0,0 +1,19 @@
+class CreateUserLocations < ActiveRecord::Migration
+  def self.up
+    execute %Q{ CREATE TABLE  `user_locations` (
+                `id` int(11) unsigned NOT NULL auto_increment,
+                `user_id` int(11) unsigned NOT NULL,
+                `current_lat` varchar(45) NOT NULL,
+                `current_long` varchar(45) NOT NULL,
+                `current_time` int(11) unsigned NOT NULL,
+                PRIMARY KEY  (`id`),
+                KEY `friend_id` (`user_id`),
+                CONSTRAINT `FK_user_locations_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+                ) ENGINE=InnoDB }
+
+  end
+
+  def self.down
+    drop_table :user_locations
+  end
+end
