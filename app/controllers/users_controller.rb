8c16f871d44da57609f6da66f13fca9f7a251893
harryrokin13 <Harry@.(none)>
2010-09-03 19:05:22 -0700

    invitation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,28 +30,75 @@ class UsersController < ApplicationController
 
   end
 
-  def accept_invite    
-    user_status = UserInvitation.status(params[:user_id],params[:email],params[:status])
-    
+  def process_invitation
+    user = User.find(params[:user_id])
+    invitation = UserInvitation.find_by_sql("select * from user_invitations where user_id=#{params[:friend_id]} and email=#{user.email}")[0]
 
-    if !user_status.blank?
+    if !params[:share].blank?
+      if params[:share].to_s == "1"
+        user_friend1 = UserFriend.find_by_sql("select * from user_friends where user_id=#{params[:user_id]} and friend_id=#{params[:friend_id]}")[0]
+        user_friend2 = UserFriend.find_by_sql("select * from user_friends where user_id=#{params[:friend_id]} and friend_id=#{params[:user_id]}")[0]
+        if user_friend1.blank?
+          user_friend1 = UserFriend.new(:user_id => params[:user_id], :friend_id => params[:friend_id], :share => params[:share])
+        else
+          user_friend1.share = params[:share]
+        end
+        if user_friend2.blank?
+          user_friend2 = UserFriend.new(:user_id => params[:friend_id], :friend_id => params[:user_id], :share => params[:share])
+        else
+          user_friend2.share = params[:share]
+        end
+        if user_friend1.save && user_friend2.save
+          invitation.update_attributes(:status => "accept")
+          render :text => "true"
+        else
+          render :text => "false"
+        end
+      else
+        invitation.update_attributes(:status => "reject")
+        render :text => "true"
+      end
+    end
 
-      friends =  [ { :name => "anil", :longitude => "77.583333", :latitude => "12.98333", :time => "10" },
-                 { :name => "kumar", :longitude=> "-76.21667", :latitude => "12.21667", :time => "44.95" } ]
-               
-
-      xml_output = buildxml_friends_list(friends)
-
-      render :xml => xml_output
-
-
-    else
-      render :text => "blab"
+    if !params[:view].blank?
+      if params[:share].to_s == "1"
+        user_friend1 = UserFriend.find_by_sql("select * from user_friends where user_id=#{params[:user_id]} and friend_id=#{params[:friend_id]}")[0]
+        user_friend2 = UserFriend.find_by_sql("select * from user_friends where user_id=#{params[:friend_id]} and friend_id=#{params[:user_id]}")[0]
+        if user_friend1.blank?
+          user_friend1 = UserFriend.new(:user_id => params[:user_id], :friend_id => params[:friend_id], :share => params[:share])
+        else
+          user_friend1.share = params[:share]
+        end
+        if user_friend2.blank?
+          user_friend2 = UserFriend.new(:user_id => params[:friend_id], :friend_id => params[:user_id], :share => params[:share])
+        else
+          user_friend2.share = params[:share]
+        end
+        if user_friend1.save && user_friend2.save
+          invitation.update_attributes(:status => "accept")
+          render :text => "true"
+        else
+          render :text => "false"
+        end
+      else
+        invitation.update_attributes(:status => "reject")
+        render :text => "true"
+      end
     end
     
+  end 
+
+  def following
+    user = UserFriend.following(params[:user_id],params[:share])
+    friends = User.find_by_sql "SELECT u.'username',f.'share' FROM users u, user_friends f WHERE u.id = f.friend_id && f.share = '1'"
+   
+    xml_output = buildxml_friends_list(friends)
+
+    render :xml => xml_output
   end
 
   def addmyfriend
+
     
   end
 
@@ -60,7 +107,7 @@ class UsersController < ApplicationController
   end
 
   def whereareyou
-    user = User.location(params[:username])
+    
     
   end
 
