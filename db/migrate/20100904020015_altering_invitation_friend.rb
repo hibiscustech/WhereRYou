38abfdc5b7c92404b6ed8aa8edaed41ff49b8d37
hibiscustech <relton@hibiscustech.com>
2010-09-07 11:58:13 +0530

    verfied - relton

--- db/migrate/20100904020015_altering_invitation_friend.rb
+++ db/migrate/20100904020015_altering_invitation_friend.rb
@@ -1,8 +1,8 @@
 class AlteringInvitationFriend < ActiveRecord::Migration
   def self.up
-    execute %Q{ ALTER TABLE `whereryou_development`.`user_invitations` ADD COLUMN `kind` ENUM('share','view') AFTER `status` }
+    execute %Q{ ALTER TABLE `user_invitations` ADD COLUMN `kind` ENUM('share','view') AFTER `status` }
 
-    execute %Q{ ALTER TABLE `whereryou_development`.`user_friends` ADD COLUMN `view` ENUM('0','1') DEFAULT 1 AFTER `share` }
+    execute %Q{ ALTER TABLE `user_friends` ADD COLUMN `view` ENUM('0','1') DEFAULT 1 AFTER `share` }
   end
 
   def self.down
