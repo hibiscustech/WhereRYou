87da5ae79abac83f8f0fed8a198d3c89cd33c806
harryrokin13 <Harry@.(none)>
2010-09-08 15:58:38 -0700

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,16 @@ class User < ActiveRecord::Base
   has_many :user_locations
   has_many :user_invitations
 
-  validates_presence_of     :username
+#  validates_presence_of     :username
   validates_length_of       :username,    :within => 3..20
   validates_uniqueness_of   :username
 
-  validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100
+  #validates_presence_of     :email
+ # validates_length_of       :email,    :within => 6..100
   validates_uniqueness_of   :email
+  #validates_format_of      :email
 
-  validates_length_of       :password,    :within => 4..40
+ # validates_length_of       :password,    :within => 4..40
 
   attr_reader :password
 
