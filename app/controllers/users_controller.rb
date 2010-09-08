a7f8c530fcfa2b688c1e609eef01986fe064af89
harryrokin13 <Harry@.(none)>
2010-09-07 19:01:08 -0700

    invites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,26 @@
 class UsersController < ApplicationController
 
+
   def register_user
+#    user1 = User.check_user(params[:username],params[:password])
+    error = ""
     user = User.new(:username => params[:username], :password => params[:password], :email => params[:email], :name => params[:name])
     if user.save!
-#      friends = [ { :name => "anil", :longitude => "77.583333", :latitude => "12.98333", :time => "10" },
-#                 { :name => "kumar", :longitude => "-76.21667", :latitude => "12.21667", :time => "44.95" } ]
-#
-#      xml_output = buildxml_friends_list(friends)
-
-      render :text => "true"
+      render :text => "your account is created"
+    else
+      if !user.errors[:username].blank?
+        error = "Username "+user.errors[:username][0]
+        render :string => error
+      end
+      if !user.errors[:email].blank?
+        error = "Email "+user.errors[:email][0]
+        render :string => error
+      end
+      if !user.errors[:password].blank?
+        error = "password "+user.errors[:password]
+        render :string => error
+      end
     end
-    render :text => "false"
   end
   
   def check_user
@@ -30,10 +40,45 @@ class UsersController < ApplicationController
 
   end
 
+  def username_invites
+   user = User.find_by_sql("select * from users where username ='#{params[:user]}'")[0]
+   friend = User.find_by_sql("select * from users where username ='#{params[:friend]}'")[0]
+   if !friend.blank?
+   invite = UserInvitation.new(:user_id => user.id, :email => friend.email,:kind => params[:kind])
+   if invite.save!
+     render :text => "Request sent successfully"
+   else
+     render :text => "Request not sent"
+   end
+   else
+     render :text => "frnd doesnt exists"
+   end
+  end
+
+  def email_invites
+    user = User.find_by_sql("select * from users where username ='#{params[:user]}'")[0]
+    friend = User.find_by_sql("select * from users where email = '#{params[:email]}'")[0]
+    if !friend.blank?
+    invite = UserInvitation.new(:user_id => user.id, :email => friend.email,:kind => params[:kind])
+    if invite.save!
+     render :text => "Request sent successfully"
+   else
+     render :text => "Request not sent"
+    end
+    else
+      render :text => "frnd doesnt exists"
+    end
+    
+   end
+
   def process_invitation
    user = User.find_by_sql("select * from users where username ='#{params[:user]}'")[0]
    friend = User.find_by_sql("select * from users where username ='#{params[:friend]}'")[0]
    invitation = UserInvitation.find_by_sql("select * from user_invitations where user_id =#{friend.id} and email ='#{user.email}'")[0]
+#   parser = XML::SaxParser.file("E:/projects/WhereRYou/tmp/status.xml")
+#   parser.parse
+    xml_status = File.read('E:/projects/WhereRYou/tmp/status.xml')
+    
 
     if !params[:share].blank?
       if params[:share].to_s == "1"
@@ -51,7 +96,7 @@ class UsersController < ApplicationController
         end
         if user_friend1.save && user_friend2.save
           invitation.update_attributes(:status => "accept")
-          render :text => "true"
+          render :xml => xml_status
         else
           render :text => "false"
         end
@@ -104,15 +149,24 @@ class UsersController < ApplicationController
 
 
   def delete_friend
-    delete_user1 = UserFriend.find_by_sql("select * from user_friends where  user_id=#{params[:user_id]} and friend_id=#{params[:friend_id]}")[0]
-    delete_user2 = UserFriend.find_by_sql("select * from user_friends where  user_id=#{params[:friend_id]} and friend_id=#{params[:user_id]}")[0]
-    if !delete_user1.blank?
-      delete_user1.destroy
-    end
-    if !delete_user2.blank?
-      delete_user2.destroy
-    end
-    
+    user = User.find_by_sql("select * from users where username ='#{params[:user]}'")[0]
+    friend = User.find_by_sql("select * from users where username ='#{params[:friend]}'")[0]
+    delete_user1 = UserFriend.find_by_sql("select * from user_friends where  user_id=#{user.id} and friend_id=#{friend.id}")[0]
+    delete_user2 = UserFriend.find_by_sql("select * from user_friends where  user_id=#{friend.id} and friend_id=#{user.id}")[0]
+     if !delete_user1.blank? && !delete_user2.blank?
+        delete_user1.update_attributes(:deleted => "yes")
+        render :text => "true"
+      else
+        render :text => "false"
+      end
+#
+#     if !delete_user2.blank? && delete_user2.save
+#       delete_user2.update_attributes(:deleted => "yes")
+#       render :text => "true"
+#      else
+#       render :text => "false"
+#      end
+# 
   end
 
 
